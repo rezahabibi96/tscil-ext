32523a636e95c3c35cdc26939fb905b1dffe3cdf
rzhb <rzhb@atlantis.dbms>
2025-05-09 09:35:15 +0900

    comply black

--- utils/utils.py
+++ utils/utils.py
@@ -20,6 +20,7 @@ class Logger(object):
     def flush(self):
         pass
 
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
 
@@ -42,9 +43,12 @@ class AverageMeter(object):
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience.
-        https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
+    https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
     """
-    def __init__(self, path, patience=5, mode='max', verbose=False, delta=0, trace_func=print):
+
+    def __init__(
+        self, path, patience=5, mode="max", verbose=False, delta=0, trace_func=print
+    ):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
@@ -69,7 +73,7 @@ class EarlyStopping:
         self.trace_func = trace_func
 
     def __call__(self, val_metric, model):
-        if self.mode == 'max':
+        if self.mode == "max":
             score = val_metric
         else:
             score = -val_metric
@@ -80,7 +84,9 @@ class EarlyStopping:
         elif score < self.best_score + self.delta:
             self.counter += 1
             if self.verbose:
-                self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+                self.trace_func(
+                    f"EarlyStopping counter: {self.counter} out of {self.patience}"
+                )
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
@@ -89,25 +95,40 @@ class EarlyStopping:
             self.counter = 0
 
     def save_checkpoint(self, val_metric, model):
-        '''Saves model when validation loss decrease.'''
+        """Saves model when validation loss decrease."""
         if self.verbose:
-            self.trace_func(f'Validation loss decreased ({self.val_metric_best:.6f} --> {val_metric:.6f}). Saving model ...')
+            self.trace_func(
+                f"Validation loss decreased ({self.val_metric_best:.6f} --> {val_metric:.6f}). Saving model ..."
+            )
         torch.save(model.state_dict(), self.path)
         self.val_metric_best = val_metric
 
 
-def seed_fixer(seed):
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-        torch.backends.cudnn.benchmark = False
-        torch.backends.cudnn.deterministic = True
+class BinaryCrossEntropy:
+    def __init__(self, dim, device):
+        self.dim = dim
+        self.device = device
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+
+    def __call__(self, logits, labels):
+        targets = ohe_label(labels, dim=self.dim, device=self.device)
+        loss = self.criterion(logits, targets)
+        return loss
 
 
-# IO
+class BinaryCrossEntropywithLogits:
+    def __init__(self, dim, device):
+        self.dim = dim
+        self.device = device
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+
+    def __call__(self, logits, target_logits):
+        targets = torch.sigmoid(target_logits)
+        loss = self.criterion(logits, targets)
+        return loss
+
+
+# ######################## IO ###########################
 def save_pickle(file, path):
     filehandler = open(path, "wb")
     pickle.dump(file, filehandler)
@@ -115,10 +136,11 @@ def save_pickle(file, path):
 
 
 def load_pickle(path):
-    file = open(path, 'rb')
+    file = open(path, "rb")
     result = pickle.load(file)
     file.close()
     return result
+
 
 def check_ram_usage():
     """
@@ -133,46 +155,11 @@ def check_ram_usage():
     return mem
 
 
+# ######################## MISC ###########################
 def boolean_string(s):
-    if s not in {'False', 'True'}:
-        raise ValueError('Not a valid boolean string')
-    return s == 'True'
-
-
-def ohe_label(label_tensor, dim, device="cpu"):
-    # Returns one-hot-encoding of input label tensor
-    # label_tensor: tensor of data's label, sth like (5000,)
-    # dim: number of classes so far
-    n_labels = label_tensor.size(0)
-    zero_tensor = torch.zeros((n_labels, dim), device=device, dtype=torch.float)
-    return zero_tensor.scatter_(1, label_tensor.reshape((n_labels, 1)), 1)
-
-
-class BinaryCrossEntropy():
-    def __init__(self, dim, device):
-        self.dim = dim
-        self.device = device
-        self.criterion = torch.nn.BCEWithLogitsLoss()
-
-    def __call__(self, logits, labels):
-        targets = ohe_label(labels, dim=self.dim, device=self.device)
-        loss = self.criterion(logits, targets)
-        return loss
-
-
-class BinaryCrossEntropywithLogits():
-    def __init__(self, dim, device):
-        self.dim = dim
-        self.device = device
-        self.criterion = torch.nn.BCEWithLogitsLoss()
-
-    def __call__(self, logits, target_logits):
-        targets = torch.sigmoid(target_logits)
-        loss = self.criterion(logits, targets)
-        return loss
-
-
-
+    if s not in {"False", "True"}:
+        raise ValueError("Not a valid boolean string")
+    return s == "True"
 
 
 def list_subtraction(l1, l2):
@@ -187,6 +174,27 @@ def nonzero_indices(bool_mask_tensor):
     return bool_mask_tensor.nonzero(as_tuple=True)[0]
 
 
+def ohe_label(label_tensor, dim, device="cpu"):
+    # Returns one-hot-encoding of input label tensor
+    # label_tensor: tensor of data's label, sth like (5000,)
+    # dim: number of classes so far
+    n_labels = label_tensor.size(0)
+    zero_tensor = torch.zeros((n_labels, dim), device=device, dtype=torch.float)
+    return zero_tensor.scatter_(1, label_tensor.reshape((n_labels, 1)), 1)
+
+
+def seed_fixer(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+
+# ######################## MATH ###########################
 def euclidean_distance(u, v):
     euclidean_distance_ = (u - v).pow(2).sum(1)
     return euclidean_distance_
@@ -194,18 +202,20 @@ def euclidean_distance(u, v):
 
 def mini_batch_deep_features(model, total_x, num):
     """
-        Compute deep features with mini-batches.
-            Args:
-                model (object): neural network.
-                total_x (tensor): data tensor.
-                num (int): number of data.
-            Returns
-                deep_features (tensor): deep feature representation of data tensor.
+    Compute deep features with mini-batches.
+        Args:
+            model (object): neural network.
+            total_x (tensor): data tensor.
+            num (int): number of data.
+        Returns
+            deep_features (tensor): deep feature representation of data tensor.
     """
     is_train = False
+
     if model.training:
         is_train = True
         model.eval()
+
     if hasattr(model, "feature"):
         model_has_feature_extractor = True
     else:
@@ -220,21 +230,27 @@ def mini_batch_deep_features(model, total_x, num):
         num_itr = num // bs + int(num % bs > 0)
         sid = 0
         deep_features_list = []
+
         for i in range(num_itr):
             eid = sid + bs if i != num_itr - 1 else num
-            batch_x = total_x[sid: eid]
+            batch_x = total_x[sid:eid]
 
             if model_has_feature_extractor:
                 batch_deep_features_ = model.feature(batch_x)
             else:
                 batch_deep_features_ = torch.squeeze(model_features(batch_x))
 
-            deep_features_list.append(batch_deep_features_.reshape((batch_x.size(0), -1)))
+            deep_features_list.append(
+                batch_deep_features_.reshape((batch_x.size(0), -1))
+            )
             sid = eid
+
         if num_itr == 1:
             deep_features_ = deep_features_list[0]
         else:
             deep_features_ = torch.cat(deep_features_list, 0)
+
     if is_train:
         model.train()
+
     return deep_features_
