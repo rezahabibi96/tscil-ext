489d25f0633115bdb8f87eb269c78c2be01fcab3
rzhb <rzhb@atlantis.dbms>
2025-05-09 09:56:30 +0900

    comply black

--- utils/metrics.py
+++ utils/metrics.py
@@ -17,31 +17,51 @@ def compute_performance(end_task_acc_arr):
     :return:                        (avg_end_acc, forgetting, avg_acc_task)
     """
     n_run, n_tasks = end_task_acc_arr.shape[:2]
-    t_coef = stats.t.ppf((1+0.95) / 2, n_run-1)  # t coefficient used to compute 95% CIs: mean +- t*SD, alpha/2 = 0.975
+    t_coef = stats.t.ppf(
+        (1 + 0.95) / 2, n_run - 1
+    )  # t coefficient used to compute 95% CIs: mean +- t*SD, alpha/2 = 0.975
 
     # compute average test accuracy and CI
-    end_acc = end_task_acc_arr[:, -1, :]                         # shape: (num_run, num_tasks)
-    avg_acc_per_run = np.mean(end_acc, axis=1)      # mean of end task accuracies per run
-    avg_end_acc = (np.mean(avg_acc_per_run), t_coef * sem(avg_acc_per_run)) if n_run > 1 else (np.mean(avg_acc_per_run),)
+    end_acc = end_task_acc_arr[:, -1, :]  # shape: (num_run, num_tasks)
+    avg_acc_per_run = np.mean(end_acc, axis=1)  # mean of end task accuracies per run
+    avg_end_acc = (
+        (np.mean(avg_acc_per_run), t_coef * sem(avg_acc_per_run))
+        if n_run > 1
+        else (np.mean(avg_acc_per_run),)
+    )
 
     # compute forgetting
     best_acc = np.max(end_task_acc_arr, axis=1)  # (num_run, num_tasks)
     final_forgets = best_acc - end_acc
 
     avg_fgt = np.mean(final_forgets[:, :-1], axis=1)
-    avg_end_fgt = (np.mean(avg_fgt), t_coef * sem(avg_fgt)) if n_run > 1 else (np.mean(avg_fgt),)
+    avg_end_fgt = (
+        (np.mean(avg_fgt), t_coef * sem(avg_fgt)) if n_run > 1 else (np.mean(avg_fgt),)
+    )
 
     # compute Avg ACC on old tasks of each task
-    acc_per_run = (np.sum(np.tril(end_task_acc_arr), axis=2) /
-                           (np.arange(n_tasks) + 1))
-    avg_acc = (np.mean(acc_per_run, axis=0), t_coef * sem(acc_per_run, axis=0)) if n_run > 1 else (np.mean(acc_per_run, axis=0),)
+    acc_per_run = np.sum(np.tril(end_task_acc_arr), axis=2) / (np.arange(n_tasks) + 1)
+    avg_acc = (
+        (np.mean(acc_per_run, axis=0), t_coef * sem(acc_per_run, axis=0))
+        if n_run > 1
+        else (np.mean(acc_per_run, axis=0),)
+    )
 
     # compute BWT+
-    bwt_per_run = (np.sum(np.tril(end_task_acc_arr, -1), axis=(1,2)) -
-                  np.sum(np.diagonal(end_task_acc_arr, axis1=1, axis2=2) *
-                         (np.arange(n_tasks, 0, -1) - 1), axis=1)) / (n_tasks * (n_tasks - 1) / 2)
+    bwt_per_run = (
+        np.sum(np.tril(end_task_acc_arr, -1), axis=(1, 2))
+        - np.sum(
+            np.diagonal(end_task_acc_arr, axis1=1, axis2=2)
+            * (np.arange(n_tasks, 0, -1) - 1),
+            axis=1,
+        )
+    ) / (n_tasks * (n_tasks - 1) / 2)
     bwtp_per_run = np.maximum(bwt_per_run, 0)
-    avg_bwtp = (np.mean(bwtp_per_run), t_coef * sem(bwtp_per_run)) if n_run > 1 else (np.mean(bwtp_per_run),)
+    avg_bwtp = (
+        (np.mean(bwtp_per_run), t_coef * sem(bwtp_per_run))
+        if n_run > 1
+        else (np.mean(bwtp_per_run),)
+    )
 
     # compute Avg Acc_cur (diagonal elements)
     diagonals = []
@@ -50,18 +70,25 @@ def compute_performance(end_task_acc_arr):
         diagonal = np.diag(matrix)
         diagonals.append(diagonal)
     diagonal_means = [np.mean(diag) for diag in diagonals]
-    avg_cur_acc = (np.mean(diagonal_means), t_coef * sem(diagonal_means)) if n_run > 1 else (np.mean(diagonal_means), )
+    avg_cur_acc = (
+        (np.mean(diagonal_means), t_coef * sem(diagonal_means))
+        if n_run > 1
+        else (np.mean(diagonal_means),)
+    )
 
     return avg_end_acc, avg_end_fgt, avg_cur_acc, avg_acc, avg_bwtp
 
 
 def compute_performance_offline(acc_multiple_run):
     n_run = acc_multiple_run.shape[0]
-
     t_coef = stats.t.ppf((1 + 0.95) / 2, n_run - 1)
 
     # compute average test accuracy and CI
-    acc = (np.mean(acc_multiple_run), t_coef * sem(acc_multiple_run)) if n_run > 1 else (np.mean(acc_multiple_run),)
+    acc = (
+        (np.mean(acc_multiple_run), t_coef * sem(acc_multiple_run))
+        if n_run > 1
+        else (np.mean(acc_multiple_run),)
+    )
     return acc
 
 
@@ -79,11 +106,12 @@ def plot_confusion_matrix(y_true, y_pred, classes, path):
     cf_matrix = cf_matrix / np.sum(cf_matrix, axis=1, keepdims=True)
     cf_matrix = np.around(cf_matrix, decimals=2)
     np.save(path, cf_matrix)
-    df_cm = pd.DataFrame(cf_matrix, index=[i for i in classes],
-                         columns=[i for i in classes])
+
+    df_cm = pd.DataFrame(
+        cf_matrix, index=[i for i in classes], columns=[i for i in classes]
+    )
 
     plt.figure(figsize=(6, 6), dpi=128)
-    s = sn.heatmap(df_cm, annot=True, fmt='g', cmap="coolwarm", center=0.3, square=True)
-    s.set(xlabel='Prediction', ylabel='Ground truth')
-    plt.savefig(path, bbox_inches='tight')
-
+    s = sn.heatmap(df_cm, annot=True, fmt="g", cmap="coolwarm", center=0.3, square=True)
+    s.set(xlabel="Prediction", ylabel="Ground truth")
+    plt.savefig(path, bbox_inches="tight")
