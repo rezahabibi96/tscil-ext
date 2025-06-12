f429761ec174b7ef8faba17604d1083ccbdac37d
rzhb <rzhb@atlantis.dbms>
2025-06-12 09:00:04 +0900

    fix learn_task

--- agents/gcppv2.py
+++ agents/gcppv2.py
@@ -5,7 +5,7 @@ from torch.optim import Adam
 import torch.nn.functional as F
 from utils.setup_elements import input_size_match
 from utils.data import Dataloader_from_numpy, extract_samples_according_to_labels
-from models.timeVAE import VariationalAutoencoderConv
+from models.gcppVAE import GCVariationalAutoencoderConv
 from utils.utils import EarlyStopping
 
 
@@ -18,16 +18,15 @@ class GenerativeClassiferPPv2(BaseLearnerGC):
         super(GenerativeClassiferPPv2, self).__init__(args)
         self.input_size = input_size_match[args.data]
         self.batch_size = args.batch_size
-        self.generators = {}
 
     def learn_task(self, task):
         (x_train, y_train), (x_val, y_val), _ = task
         self.before_task(y_train)
 
         if self.verbose:
-            print(f"Creating the shared encoder")
+            print(f"Creating the base generator with the shared encoder")
 
-        vae = VariationalAutoencoderConv(
+        generator = GCVariationalAutoencoderConv(
             seq_len=self.input_size[0],
             feat_dim=self.input_size[1],
             latent_dim=self.args.feature_dim,  # 2 for visualization
@@ -36,15 +35,17 @@ class GenerativeClassiferPPv2(BaseLearnerGC):
             recon_wt=self.args.recon_wt,
         )
 
+        ckpt_path_g = self.ckpt_path.replace("/ckpt", f"/generator_ckpt")
         epochs_g = self.args.epochs_g
-        ckpt_path_g = self.ckpt_path.replace("/ckpt", f"/vae_ckpt_{id}")
 
         for id in self.classes_in_task:
             if self.verbose:
                 print(f"Creating the disentangled decoder {id}")
 
-            vae.create_decoder(id)
-            optimizer_g = Adam(vae.parameters(), lr=self.args.lr_g, betas=(0.9, 0.999))
+            generator.create_decoder(id)
+            optimizer_g = Adam(
+                generator.parameters(), lr=self.args.lr_g, betas=(0.9, 0.999)
+            )
 
             if self.verbose:
                 print(f"Training the generator {id}")
@@ -75,5 +76,38 @@ class GenerativeClassiferPPv2(BaseLearnerGC):
                     x = x.to(self.device)
                     x_ = None
 
+                    # generator's input should be (N, L, C)
+                    rnt = 1 / (self.task_now + 1) if self.args.adaptive_weight else 0.5
+                    generator_loss_dict = generator.train_a_batch(
+                        x=x.transpose(1, 2),
+                        optimizer=optimizer_g,
+                        decoder_id=id,
+                        x_=x_,
+                        rnt=rnt,
+                    )
+
+                train_mse_loss, train_kl_loss = generator.evaluate(train_dataloader, id)
+                # Validate on val set for early stop
+                val_mse_loss, val_kl_loss = generator.evaluate(val_dataloader, id)
+
+                if self.verbose:
+                    print(
+                        "Epoch {}/{}: Recons Loss = {}, KL Divergence = {}".format(
+                            epoch + 1,
+                            epochs_g,
+                            train_mse_loss,
+                            train_kl_loss,
+                        )
+                    )
+
+                early_stopping(val_mse_loss, generator)
+                if early_stopping.early_stop:
+                    if self.verbose:
+                        print("Early stopping")
+                    break
+
+        self.after_task(None, None)
+
     def evaluate(self, task_stream, path=None):
+        print("OK")
         pass
